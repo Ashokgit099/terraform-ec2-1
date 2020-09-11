a1505e1845d04da8ddb0f3b9610b8577d9e43b89
Ashokgit099 <47042444+Ashokgit099@users.noreply.github.com>
2020-09-11 19:09:28 -0400

    Update sgf.tf

--- east/how to create ec2 instance/sgf.tf
+++ east/how to create ec2 instance/sgf.tf
@@ -7,13 +7,13 @@ resource "aws_security_group" "AshokSG" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "[0.0.0.0/0]"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = "[10.0.0.0/16]"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   ingress {
@@ -27,19 +27,19 @@ resource "aws_security_group" "AshokSG" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "[10.0.0.0/16]"
+    cidr_blocks = ["10.0.0.0/16"]
     }
     
   egress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = "[10.0.0.0/16]"
+    cidr_blocks = ["10.0.0.0/16"]
   }
   egress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = "[10.0.0.0/16]"
+    cidr_blocks = ["10.0.0.0/16"]
     }
 }
