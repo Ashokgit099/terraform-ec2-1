23a551acf1ad3a09a5bcafb05f8aaa96ca39d52e
Ashokgit099 <47042444+Ashokgit099@users.noreply.github.com>
2020-09-11 18:36:48 -0400

    Update sgf.tf

--- east/how to create ec2 instance/sgf.tf
+++ east/how to create ec2 instance/sgf.tf
@@ -1,13 +1,13 @@
 resource "aws_security_group" "AshokSG" {
   name        = "AshokSG"
   description = "Create a Security group for create a EC2 instance"
-  vpc_id      = "default"
+  vpc_id      = "${var.vpc}"
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "[10.0.0.0/16]"
+    cidr_blocks = "[10.0.0.0/0]"
   }
   ingress {
     from_port   = 80
